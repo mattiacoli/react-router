b993f1c7facb324f2e3abda05691bd725e8adba9
mattiacoli <mattia.coli@aol.com>
2025-04-01 13:13:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+
+  return (
+    <>
+      <header>
+
+        <nav className="navbar bg-primary p-4" data-bs-theme="dark">
+          <div className="logo">
+            Router<b>React</b>
+          </div>
+
+
+          <div className="links_nav">
+            <ul className="nav gap-2">
+              <li className="nav-item">
+                <NavLink to={'/'}>Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to={'/about'}>Chi Siamo</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to={'/posts'}>Posts</NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+
+      </header>
+
+    </>
+  )
+}
